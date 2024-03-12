ccabc8f62c3f3258c73b1e8932928c54396684db
Ved Recharla <94962003+mVedr@users.noreply.github.com>
2024-03-12 13:37:07 +0530

    Added mail id

--- notifier/config.py
+++ notifier/config.py
@@ -2,7 +2,7 @@ import asyncio
 
 KAFKA_BOOTSTRAP_SERVERS="localhost:9092"
 KAFKA_TOPIC="notification"
-SENDER_EMAIL="vedrecharla@gmail.com"
+SENDER_EMAIL="YOUR_MAIL_ID"
 SENDER_PASSWORD="YOUR_APP_PASSWORD"
 SMTP_SERVER="smtp.gmail.com"
 SMTP_PORT=587
