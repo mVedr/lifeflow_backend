d440aff7c6ba95d885326b17089d73c7fb275717
ved2805 <vedrecharla@gmail.com>
2024-03-15 12:56:05 +0530

    removed tomtom

--- db_server/config.py
+++ db_server/config.py
@@ -3,7 +3,6 @@ import asyncio
 KAFKA_BOOTSTRAP_SERVERS= "localhost:9092"
 NOTIFICATION_TOPIC="notification"
 VERIFICATION_TOPIC="verification"
-TOMTOM_API_KEY="Cm4orOUv8dAnSjmvVYCc1OAV2R9omkNI"
 TRANSACTION_TOPIC="transaction"
 TOMTOM_API_KEY="YOUR_TOMTOM"
 loop = asyncio.get_event_loop()
